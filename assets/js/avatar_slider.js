ae700a26798b4486dbf3dc7189dee4b9cae0e73e
vtu81 <48283858+vtu81@users.noreply.github.com>
2023-12-14 12:22:39 -0600

    update

--- assets/js/avatar_slider.js
+++ assets/js/avatar_slider.js
@@ -1,5 +1,6 @@
 const imgElement = document.getElementById('avatar_div');
 let avatar_images = [
+    'nips23_headshot.jpg',
     'xie.tinghao.square.png',
     // '0401.jpeg',
     // 'selfie_1018.png',
