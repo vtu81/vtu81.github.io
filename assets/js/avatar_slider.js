2ae719771b807ae05a2573bf11035246a5b8c24f
vtu81 <48283858+vtu81@users.noreply.github.com>
2025-09-30 16:57:41 -0400

    update

--- assets/js/avatar_slider.js
+++ assets/js/avatar_slider.js
@@ -1,22 +1,25 @@
 const imgElement = document.getElementById('avatar_div');
 let avatar_images = [
-    'iclr24_headshot.jpg',
-    'nips23_headshot.jpg',
-    'xie.tinghao.square.png',
+    'ECE-headshot-2025-square.jpg',
+
+    // 'iclr24_headshot.jpg',
+    // 'nips23_headshot.jpg',
+    // 'xie.tinghao.square.png',
+    // 'grad_0.jpg',
+    // 'grad_2.jpg',
+    // 'IMG_7818.png',
+    // 'IMG_7826.png',
+    // 'IMG_8036.png',
+    // 'IMG_8667.png',
+    // '2023.0920.square.png',
+
     // '0401.jpeg',
     // 'selfie_1018.png',
     // '0430.jpg',
     // 'vtu-icon-beach.png',
-    'grad_0.jpg',
     // 'grad_1.jpg',
-    'grad_2.jpg',
     // 'IMG_3396.png',
-    'IMG_7818.png',
-    'IMG_7826.png',
-    'IMG_8036.png',
-    'IMG_8667.png',
     // 'IMG_8669.png',
-    '2023.0920.square.png',
 ];
 let num_images = avatar_images.length;
 let index = 0;
